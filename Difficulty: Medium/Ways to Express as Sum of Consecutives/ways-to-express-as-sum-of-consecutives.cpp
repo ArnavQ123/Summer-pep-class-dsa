0db8afa317a5c0cf43e7856ad19958b0b01b3f12
Arnav Khandelwal <149396215+ArnavQ123@users.noreply.github.com>
2026-07-10 10:53:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Ways to Express as Sum of Consecutives/ways-to-express-as-sum-of-consecutives.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int getCount(int n) {
+        int ans = 0;
+        for (long long k = 2; k * (k + 1) / 2 <= n; k++) {
+            long long rem = n - k * (k - 1) / 2;
+            if (rem > 0 && rem % k == 0) ans++;
+        }
+        return ans;
+    }
+};
