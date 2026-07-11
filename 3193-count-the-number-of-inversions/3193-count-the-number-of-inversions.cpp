3d8cf3843939d3faf09be05e43719fcddb88a345
Arnav Khandelwal <149396215+ArnavQ123@users.noreply.github.com>
2026-07-11 13:49:58 +0530

    Time: 21 ms (94.37%), Space: 40.9 MB (61.93%) - LeetHub

--- 3193-count-the-number-of-inversions/3193-count-the-number-of-inversions.cpp
+++ 3193-count-the-number-of-inversions/3193-count-the-number-of-inversions.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
-    int numberOfPermutations(int n, vector<vector<int>>& r) {
-        const int MOD=1e9+7;
-        vector<int>req(n,-1);
-        for (auto&v:r)
-            req[v[0]]=v[1];
-        if(req[0]>0) return 0;
-        req[0]=0;
-        int m=*max_element(req.begin(),req.end());
-        vector<vector<int>>dp(n,vector<int>(m+1));
-        dp[0][0]=1;
-        for(int i=1;i<n;i++){
-            for(int j=0;j<=m;j++){
-                if(req[i]!=-1 &&j!=req[i]) continue;
-                for(int k=0;k<=min(i,j);k++)
-                    dp[i][j]=(dp[i][j]+dp[i-1][j-k])%MOD;
+    int numberOfPermutations(int n, vector<vector<int>>& requirements) {
+        const int MOD = 1e9 + 7;
+        vector<int> req(n, -1);
+
+        for (auto &v : requirements)
+            req[v[0]] = v[1];
+
+        if (req[0] > 0) return 0;
+        req[0] = 0;
+
+        int m = *max_element(req.begin(), req.end());
+
+        vector<vector<int>> dp(n, vector<int>(m + 1));
+        dp[0][0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            vector<int> pre(m + 1);
+            pre[0] = dp[i - 1][0];
+            for (int j = 1; j <= m; j++)
+                pre[j] = (pre[j - 1] + dp[i - 1][j]) % MOD;
+
+            for (int j = 0; j <= m; j++) {
+                if (req[i] != -1 && j != req[i]) continue;
+
+                dp[i][j] = pre[j];
+                if (j > i) {
+                    dp[i][j] = (dp[i][j] - pre[j - i - 1] + MOD) % MOD;
+                }
             }
         }
-        return dp[n-1][req[n-1]];
+
+        return dp[n - 1][req[n - 1]];
     }
 };
