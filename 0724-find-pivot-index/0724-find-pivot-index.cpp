4555e357046e065abc63f84d23f1ab5178f30474
Arnav Khandelwal <149396215+ArnavQ123@users.noreply.github.com>
2026-08-22 23:15:12 +0530

    Time: 0 ms (100%), Space: 35.8 MB (55.54%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int rs=0;
+        for(int num:nums){
+            rs+=num;
+        }
+        int ls=0;
+        for(int i=0;i<nums.size();i++){
+            rs-=nums[i];
+            if(ls==rs){
+                return i;
+            }
+            ls+=nums[i];
+        }
+    return -1;
+    }
+};
